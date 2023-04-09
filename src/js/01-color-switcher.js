00046653d129f63e3bc3d534389f03961a1fa010
dimside <dimside29@gmail.com>
2023-04-09 15:22:44 +0300

    Solved-first-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startButtonEl: document.querySelector('[data-start]'),
+  stopButtonEl: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let idInterval = null;
+const CHANGE_COLOR_DELAY = 1000;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+refs.startButtonEl.addEventListener('click', onStartBtnClick);
+refs.stopButtonEl.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(evt) {
+  idInterval = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+  evt.target.setAttribute('disabled', '');
+}
+
+function onStopBtnClick() {
+  clearInterval(idInterval);
+  refs.startButtonEl.removeAttribute('disabled');
+}
